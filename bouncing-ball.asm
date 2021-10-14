dc1692177bcd86972ce0a07102d9736edc76ab97
Matt Rossner <mrossner@salesforce.com>
2021-10-14 19:27:11 +0200

    now detects left wall collision - getting closer

--- bouncing-ball.asm
+++ bouncing-ball.asm
@@ -38,140 +38,172 @@
 ; We'll store trajectory in memory address $02
 
 game:
-JSR initBall
-JSR initTrajectory
-JSR loop
+    JSR initBall
+    JSR initTrajectory
+    JSR loop
 
 loop:
-JSR drawBall
-JSR update
-JSR spinWheels
-; infinite loop
-JMP loop
+    JSR drawBall
+    JSR update
+    JSR detectBorder
+    JSR spinWheels
+    ; infinite loop
+    JMP loop
 
 
 initBall:
-; we'll store the X/Y coordiantes in memory locations $00 and $01 respectively
-; let's give it a random starting position within the boundaries of $0200 - $05ff
-; why is loading $fe random? Not sure... TBC
-; Since we actually have 4 pages of 00-ff, then 2-5 (see the Gist above)
-; we can generate a position and a page
-; First random is any single byye
-LDA $fe
-;LDA #$f0 ;DEBUG ONLY - force value
-STA $00
-; now load Y which is a page from 2-5
-LDA $fe
-; now let's bitmask this
-; so really we want random 0-3, then we can shift it over? ot just add 2
-; bitmask is % 0000 0011
-AND #$02
-ADC #02
+    ; we'll store the X/Y coordiantes in memory locations $00 and $01 respectively
+    ; let's give it a random starting position within the boundaries of $0200 - $05ff
+    ; why is loading $fe random? Not sure... TBC
+    ; Since we actually have 4 pages of 00-ff, then 2-5 (see the Gist above)
+    ; we can generate a position and a page
+    ; First random is any single byye
+    LDA $fe
+    LDA #$a7 ;DEBUG ONLY - force value
+    STA $00
+    ; now load Y which is a page from 2-5
+    LDA $fe
+    ; now let's bitmask this
+    ; so really we want random 0-3, then we can shift it over? ot just add 2
+    ; bitmask is % 0000 0011
+    AND #$02
+    ADC #02
 
-;LDA #3 ;DEBUG ONLY - force value
+    LDA #3 ;DEBUG ONLY - force value
 
-STA $01
+    STA $01
 
-; set last location empty
-LDX #0
-STX $03
-LDX #2
-STX $04
+    ; set last location empty
+    LDX #0
+    STX $03
+    LDX #2
+    STX $04
 
-RTS
+    RTS
 
 initTrajectory:
-; random from 0-3
-LDA $fe
-AND #02
-;LDA #0 ; DEBUG ONLY - force value
-STA $02
+    ; random from 0-3
+    LDA $fe
+    AND #02
+    LDA #3 ; DEBUG ONLY - force value
+    STA $02
+
+    RTS    
 
 drawBall:
-;clear the Y register
-LDY #0
-; clear any previous position
-LDA #0
-STA ($03),Y
+    ;clear the Y register
+    LDY #0
+    ; clear any previous position
+    LDA #0
+    STA ($03),Y
 
-; draw the ball
-;set color
-LDA #01
+    ; draw the ball
+    ;set color
+    LDA #01
 
-STA ($00),Y
-RTS
-
-;clearBall:;
-
-;LDA #0
-;LDY #0
-;STA ($00),Y
-;RTS
+    STA ($00),Y
+    RTS
 
 update:
-;store the last position
-LDX $00
-STX $03
-LDX $01
-STX $04
-; TODO need todetermine collisions
+    ;store the last position
+    LDX $00
+    STX $03
+    LDX $01
+    STX $04
+    ; TODO need todetermine collisions
+    ; TODO change direction to use 2 bits
+    LDX $02
+    CPX #0
+    BEQ upLeft
+    CPX #1
+    BEQ upRight
+    CPX #2
+    BEQ downRight
+    CPX #3
+    BEQ downLeft
 
-LDX $02
-CPX #0
-BEQ upLeft
-CPX #1
-BEQ upRight
-CPX #2
-BEQ downRight
-CPX #3
-BEQ downLeft
-; TODO up and down are reversed
-; down page works, up page not - to be investigated
     upLeft:
-    DEC $00 ; move x 1 to the left
-    ; move y 1 up
-    JSR upPage
-    RTS
+        DEC $00 ; move x 1 to the left
+        ; move y 1 up
+        JSR decPage
+        RTS
 
     upRight:
-    INC $00
-    JSR upPage
-    RTS
+        INC $00
+        JSR decPage
+        RTS
 
     downRight:
-    INC $00
-    JSR downPage
-    RTS
+        INC $00
+        JSR incPage
+        RTS
 
     downLeft:
 
-    DEC $00
-    JSR downPage
-    RTS
+        DEC $00
+        JSR incPage
+        RTS
 
-upPage:
-    ; we need to add 32/0x20 in this case
-    ; if carry flag is set we need to move to the next page
-    LDA $00 ; we still load 00 into memory
-    CLC ; clear carry first
-    ADC #$20
-    STA $00 ; store it back
-    BCS nextPage
-    RTS
-    nextPage:
-    INC $01 ; next page
-    RTS
+        incPage:
+            ; we need to add 32/0x20 in this case
+            ; if carry flag is set we need to move to the next page
+            LDA $00 ; we still load 00 into memory
+            CLC ; clear carry first
+            ADC #$20
+            STA $00 ; store it back
+            BCS nextPage
+            RTS
+            nextPage:
+                INC $01 ; next page
+                RTS
 
-downPage:
-    LDA $00 ; we still load 00 into memory
-    CLC ; clear carry first
-    SBC #$20
-    STA $00 ; store it back
-    BCS prevPage
+        decPage:
+            LDA $00 ; we still load 00 into memory
+            SBC #$20
+            STA $00 ; store it back
+            BCC prevPage
+            RTS
+            prevPage:
+                DEC $01 ; preivous page
+                RTS
+
+
+detectBorder:
+    ; hopefully we can figure this out with some bit masking
+    ; left borders are 
+    ; $0x00
+    ; $0x20 
+    ; $0x40 
+    ; ...
+    ; $0xe0 
+    ; so actually we only about the least significant part which is stored in $00
+    ; We can already mask way the lowest 4 bits % 11110000 / $f0
+    LDX $00
+    TXA
+    AND #$0f
+    BEQ checkFurther
     RTS
-    prevPage:
-    DEC $01 ; next page
-    RTS
+    checkFurther:
+        AND #$f0
+        ; now we can check for even
+        ; we can shift this 4 times to the right
+        LSR A
+        LSR A
+        LSR A
+        LSR A
+        ; now we logical AND with 1 to see if it's even or not
+        AND #01
+        ; if it's a border we change direction
+        BEQ changeDirLR
+        RTS
+
+        changeDirLR:
+            DEC $02 
+            RTS
+;    changeDirLR:
+;        LDX $02 ; direction stored in memory 2
+
+
 
 ; from that Gist
 spinWheels:
